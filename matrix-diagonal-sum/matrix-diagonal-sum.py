e7d675c332ca45732f64cf834d59638548c25619
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2021-09-22 23:33:34 -0700

    Update matrix-diagonal-sum.py

--- matrix-diagonal-sum/matrix-diagonal-sum.py
+++ matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -16,6 +16,8 @@ class Solution:
             res = res + mat[i][j]
             j = j- 1
         
+        
+#         since the element which is common in both diagonals, is counted only once. so we subtract that element from the total result 
         if len(mat)%2 !=0:
             res = res - mat[len(mat)//2][len(mat)//2]
         return res
