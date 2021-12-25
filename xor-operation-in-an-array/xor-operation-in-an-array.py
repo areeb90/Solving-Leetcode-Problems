91f9dfafc1d1cff0416ace73e8a9665168c6679b
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2021-12-25 04:24:39 -0800

    Update xor-operation-in-an-array.py

--- xor-operation-in-an-array/xor-operation-in-an-array.py
+++ xor-operation-in-an-array/xor-operation-in-an-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         x= 0
-        #We dont actually need to create the nums variable as just the output number is required!
-		#We create a variable to store the output, i have gave it the default value of 0, because 0 is immune to XOR operation.
+        # dont actually need to create the nums variable as just the output number is required!
+		#creates a variable to store the output, i have gave it the default value of 0, because 0 is immune to XOR operation.
         for i in range(n):
 #             calculating XOR operations directly
             x = x^(start+2*i) 
