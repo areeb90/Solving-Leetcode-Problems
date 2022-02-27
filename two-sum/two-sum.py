ef41740933a5b8997c91cfa87b7ad909c9c8b468
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2022-02-27 11:44:24 +0500

    Time: 64 ms (86.00%), Space: 15.2 MB (57.63%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,29 +1,26 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         lPointer , rPointer = 0 , len(nums)-1
-        
-#         while rPointer > lPointer:
-#             currentSum = nums[lPointer] + nums[rPointer]
-#             if currentSum > target :
-#                 rPointer -= 1
-#             elif currentSum < target:
-#                 lPointer +=1
-#             else:
-#                 return [lPointer  , rPointer]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:  
+        nums_dict = {}
+        for i in range(len(nums)):
+            cur_num = nums[i]
+            nums_dict[cur_num] = i
             
-#         return []
+        for i in range(len(nums)):
+            cur_num = nums[i]
+            check_num = target - cur_num
+            if check_num in nums_dict:
+                if nums_dict[check_num] != i:
+                    return(i , nums_dict[check_num])
+            
             
     
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                curSum = nums[i]+nums[j]
-                if curSum == target:
-                    return [i , j]
+#         for i in range(0, len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 curSum = nums[i]+nums[j]
+#                 if curSum == target:
+#                     return [i , j]
                 
 
-    
-        
-    
     
 a = Solution()
 a.twoSum([2,3,4,8,4,7,5,1], 12)
