dc1041b78507eb7551c0c4345ed604f44ceed06f
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2022-02-28 14:11:58 +0500

    Time: 204 ms (89.69%), Space: 15.8 MB (41.85%) - LeetHub

--- set-mismatch/set-mismatch.py
+++ set-mismatch/set-mismatch.py
@@ -1,15 +1,40 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        result=[]
-        Set = []
-        for i in range(len(nums)):
-            if nums[i] in Set:
-                result.append(nums[i])
-            else:
-                Set.append(nums[i])
         
-        for i in range(1, len(nums)+1):
-            if i not in nums:
-                result.append(i)
-                return result
+        dic = {}
+        for i in nums:
+            dic[i] = dic.get(i, 0)+1
             
+            
+        duplicate =  missing = 0
+        for i in range(1 , len(nums)+1):
+            if i not in dic:
+                missing = i
+            elif dic[i] == 2:
+                duplicate = i
+            
+            if missing and duplicate :
+                return [duplicate , missing]
+        
+        
+        
+        
+        
+        
+#         res = []
+#         non_d = []
+#         for i in range(len(nums)):
+            
+#             if nums[i] in non_d:
+#                 res.append(nums[i])
+#             else:
+#                 non_d.append(nums[i])
+                
+#         for i in range(1, len(nums)+1):
+#             if i not in nums:
+#                 res.append(i)
+                
+#                 return res
+
+                
+                # result = [duplicate , miss]
