7a564ebb0b1cb0c017fc8a1966324a90e02837f9
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2022-03-05 23:49:21 +0500

    Time: 192 ms (69.89%), Space: 15.4 MB (78.66%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,13 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic={}
-        for n in nums:
-            if n not in dic:
-                dic[n]=1
-            else:
-                dic[n] +=1
+        nums = sorted(nums)
+        midIndex=len(nums)//2
+        return nums[midIndex]
+        
+        
+#         dic={}
+#         for n in nums:
+#             if n not in dic:
+#                 dic[n]=1
+#             else:
+#                 dic[n] +=1
             
-            if dic[n] > (len(nums)/2):
-                return n
+#             if dic[n] > (len(nums)/2):
+#                 return n
             
             
