6fea7caaf3a556a58eae406fd891aaec4479ea02
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2022-03-11 23:55:30 +0500

    Time: 68 ms (37.42%), Space: 14.9 MB (88.88%) - LeetHub

--- fizz-buzz/fizz-buzz.py
+++ fizz-buzz/fizz-buzz.py
@@ -1,13 +1,17 @@
 class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
+    def fizzBuzz(self, n: int) -> List[str]:        
         l = []
         for i in range(1 , n+1):
             if (i%3 == 0 and i%5==0):
-                l.insert(i-1 , "FizzBuzz")
+                l.append("FizzBuzz")
+                # l.insert(i-1 , "FizzBuzz")
             elif (i%3 == 0):
-                l.insert(i-1, "Fizz")
+                l.append("Fizz")
+                # l.insert(i-1, "Fizz")
             elif (i%5==0):
-                l.insert(i-1 , "Buzz")
+                l.append("Buzz")
+                # l.insert(i-1 , "Buzz")
             else:
-                l.insert(i-1,  str(i))
+                l.append(str(i))
+                # l.insert(i-1,  str(i))
         return l
