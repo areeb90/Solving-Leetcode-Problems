e012943617fe5a3bd1ed807a28e1b0e4f3a973c8
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2022-10-04 00:32:43 +0500

    Time: 47 ms (70.29%), Space: 13.9 MB (66.61%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,38 +1,32 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
+        c = Counter(s)                      #count the # of letters in a string
+        count=0
+        singular_or_oddLetter=0
+        for v in c:      
+            
+            if c[v]%2 ==0:                  #check if divisible by 2 add the # of objects with the count
+               count += c[v]
+            elif c[v]%2 == 1 :             #if odd, then do -1 make it even to get add in count
+                count += c[v]-1
+                singular_or_oddLetter=1
+        if singular_or_oddLetter == 1:  #items having value==1 will add 1 to the total count
+            count+=1
+        return count
         
-        ans = tmp = len(s)   #Use tmp here to store original value of len(s)
-        for v in collections.Counter(s).values():
-            ans -= v % 2   # if v % 2 == 1 then ans will be deducted
-        if tmp != ans:
-            ans += 1   # if we deducted something we will need to add back the one odd char we can handle
-        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         ans = tmp = len(s)                            #Use tmp here to store original value of len(s)
+#         for v in collections.Counter(s).values():
+#             ans -= v % 2                              # if v % 2 == 1 then ans will be deducted
+#         if tmp != ans:
+#             ans += 1                                  # if we deducted something we will need to add back the one odd char we can handle
+#         return ans
     
-        # count=len(s)
-        # l = []
-        # counter={}
-        # for letter in s:
-        #     if letter not in counter:
-        #         counter[letter]=0
-        #     counter[letter]+=1
-        # for v in counter.values():
-        #     if v%2 ==1:
-        #         count-=1
-            
-                
-            
-            
-            
-        #     if count(int(split[i])) % 2 == 0:
-        #         l.append(split[i])
-        #     else:
-        #         count +=1
-        # total = len(l)+count
-        # return total
-                
-            
-        #     if (count(split[i])//2==0):
-        #         count +=1
-        #     else:
-        #         count+=1
-        # return count
