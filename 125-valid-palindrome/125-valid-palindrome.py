22d496f27c57cfe9fca9c95e8958bfe1f564a5bf
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2022-10-05 19:30:30 +0500

    Time: 105 ms (27.93%), Space: 14.8 MB (35.10%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -19,3 +19,34 @@ class Solution:
                     return False
             
         return True
+    
+    
+    
+#             l , r = 0 , len(s)-1                    
+#         while l<r:
+#             while l<r and not s[l].isalnum():
+#                 l +=1
+#             while l<r and not s[r].isalnum():
+#                 r -=1
+#             if s[l].lower() != s[r].lower():
+#                 return False
+#             l+=1
+#             r-=1
+#         return True
+
+# ___________________________________________________________________
+
+    
+        # s=[i for i in s.lower() if i.isalnum()]
+        # return s == s[::-1]
+        # __________________________________________
+        
+#         stringg = s.replace(" ","")
+#         revString = stringg[::-1]
+#         if stringg == revString:
+            
+#             return True
+#         else:
+#             return False
+        
+                
