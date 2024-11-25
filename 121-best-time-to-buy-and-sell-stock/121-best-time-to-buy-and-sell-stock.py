4ceac427f16ac517f1e79e4814584f20e4411971
areeb90 <areebbinazim360@gmail.com>
2024-11-26 00:29:52 +0500

    Runtime 106 ms Beats 42.57% Memory 26.04 MB Beats 47.12%

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        l, r = 0, 1
+        while (r < len(prices)):
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                maxProfit = max(profit, maxProfit)
+            else: 
+                l = r
+            r += 1
+        return maxProfit
